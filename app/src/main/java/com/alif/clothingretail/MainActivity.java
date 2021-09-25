6a5ca84a56037e9630c0ec2db79693dbdc4e4588
alifdevz <alifm.devz@gmail.com>
2021-09-25 14:05:32 +0800

    Change FButton to Button and delete FButton dependency

--- app/src/main/java/com/alif/clothingretail/MainActivity.java
+++ app/src/main/java/com/alif/clothingretail/MainActivity.java
@@ -7,27 +7,27 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnSignIn, btnSignUp;
+//    Button btnSignIn, btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnSignIn = findViewById(R.id.btn_sign_in);
-        btnSignUp = findViewById(R.id.btn_sign_up);
+//        btnSignIn = findViewById(R.id.btn_sign_in);
+//        btnSignUp = findViewById(R.id.btn_sign_up);
 
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+//        btnSignIn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//
+//        btnSignUp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
     }
 }
