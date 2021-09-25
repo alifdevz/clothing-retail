2002b1672fc39c8e6988c458b90f0061639cce46
alifdevz <alifm.devz@gmail.com>
2021-09-25 11:28:47 +0800

    Added new font + Added button sign up and button sign in

--- app/src/main/java/com/alif/clothingretail/MainActivity.java
+++ app/src/main/java/com/alif/clothingretail/MainActivity.java
@@ -3,12 +3,31 @@ package com.alif.clothingretail;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnSignIn, btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btn_sign_in);
+        btnSignUp = findViewById(R.id.btn_sign_up);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
