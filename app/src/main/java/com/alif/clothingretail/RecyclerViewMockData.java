58366f2801881b64e192e13e1233a620487e6d4b
alifdevz <alifm.devz@gmail.com>
2021-09-28 21:06:17 +0800

    Added one more mock data in RecyclerviewMockData.java

--- app/src/main/java/com/alif/clothingretail/RecyclerViewMockData.java
+++ app/src/main/java/com/alif/clothingretail/RecyclerViewMockData.java
@@ -4,11 +4,13 @@ public class RecyclerViewMockData {
     public static int[] itemImage = {
             R.drawable.example_clothing_item,
             R.drawable.example_image2,
-            R.drawable.example_image3
+            R.drawable.example_image3,
+            R.drawable.example_image4
     };
     public static String[] itemName = {
             "Pakaian anak lengkap",
             "Baju merah putih",
-            "Baju lengan panjang"
+            "Baju lengan panjang",
+            "Pakaian anak laki-laki warna putih"
     };
 }
