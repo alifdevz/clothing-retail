07288667e9fc52a8ade1e1d5039f6831cb44a5dd
alifdevz <alifm.devz@gmail.com>
2021-09-28 10:27:21 +0800

    Delete unused items and fragments in navigation drawes

--- app/src/main/java/com/alif/clothingretail/HomeActivity.java
+++ app/src/main/java/com/alif/clothingretail/HomeActivity.java
@@ -45,7 +45,7 @@ public class HomeActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_items, R.id.nav_cart, R.id.nav_orders, R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_items, R.id.nav_cart, R.id.nav_orders)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
