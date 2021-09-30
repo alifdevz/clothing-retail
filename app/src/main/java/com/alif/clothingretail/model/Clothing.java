04ab1fa1e1cdf7e68ca604730cfd0af886bc77d6
alifdevz <alifm.devz@gmail.com>
2021-09-30 21:17:45 +0800

    Added description property in Clothing object

--- app/src/main/java/com/alif/clothingretail/model/Clothing.java
+++ app/src/main/java/com/alif/clothingretail/model/Clothing.java
@@ -4,14 +4,16 @@ public class Clothing {
     private String name;
     private String image;
     private String price;
+    private String description;
 
     public Clothing() {
     }
 
-    public Clothing(String name, String image, String price) {
+    public Clothing(String name, String image, String price, String description) {
         this.name = name;
         this.image = image;
         this.price = price;
+        this.description = description;
     }
 
     public String getName() {
@@ -36,5 +38,13 @@ public class Clothing {
 
     public void setPrice(String price) {
         this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
