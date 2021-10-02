196f94c8e07c3c1dcbbf74821e5b640e84d1de04
alifdevz <alifm.devz@gmail.com>
2021-10-02 08:46:47 +0800

    Added ItemClickListener interface to ItemsViewHolder

--- app/src/main/java/com/alif/clothingretail/viewholder/ItemsViewHolder.java
+++ app/src/main/java/com/alif/clothingretail/viewholder/ItemsViewHolder.java
@@ -10,17 +10,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.alif.clothingretail.ClothingDetailsActivity;
 import com.alif.clothingretail.R;
 import com.alif.clothingretail.RecyclerViewMockData;
+import com.alif.clothingretail.interfaces.ItemClickListener;
 import com.squareup.picasso.Picasso;
 
 public class ItemsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     public ImageView itemImage;
     public TextView itemName;
+    private ItemClickListener itemClickListener;
 
-    public ItemsViewHolder(View view) {
-        super(view);
-        itemImage = view.findViewById(R.id.clothing_item_image);
-        itemName = view.findViewById(R.id.clothing_item_name);
-        view.setOnClickListener(this);
+    public ItemsViewHolder(View itemView) {
+        super(itemView);
+        itemImage = itemView.findViewById(R.id.clothing_item_image);
+        itemName = itemView.findViewById(R.id.clothing_item_name);
+        itemView.setOnClickListener(this);
     }
 
     public void bindView(int position) {
@@ -32,9 +34,14 @@ public class ItemsViewHolder extends RecyclerView.ViewHolder implements View.OnC
         itemName.setText(RecyclerViewMockData.itemName[position]);
     }
 
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
     @Override
     public void onClick(View view) {
-        Intent clothingDetailsIntent = new Intent(view.getContext(), ClothingDetailsActivity.class);
-        view.getContext().startActivity(clothingDetailsIntent);
+        // Intent clothingDetailsIntent = new Intent(view.getContext(), ClothingDetailsActivity.class);
+        // view.getContext().startActivity(clothingDetailsIntent);
+        itemClickListener.onClick(view , getAbsoluteAdapterPosition(), false);
     }
 }
