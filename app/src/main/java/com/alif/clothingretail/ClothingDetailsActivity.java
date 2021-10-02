d3b643de243e2285b3d80594ae94c22e4738e91c
alifdevz <alifm.devz@gmail.com>
2021-10-02 10:05:58 +0800

    Get data from Firebase to populate views in ClothingDetailsActivity

--- app/src/main/java/com/alif/clothingretail/ClothingDetailsActivity.java
+++ app/src/main/java/com/alif/clothingretail/ClothingDetailsActivity.java
@@ -1,23 +1,34 @@
 package com.alif.clothingretail;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.alif.clothingretail.model.Clothing;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 public class ClothingDetailsActivity extends AppCompatActivity {
-    private ImageView clothingItemImage;
+    private ImageView clothingImage;
+    private TextView clothingName, clothingPrice, clothingDescription;
+    private TextView number;
+    private Button btnIncrement, btnDecrement;
 
     private FirebaseDatabase database;
     private DatabaseReference clothing;
     private FirebaseRecyclerOptions<Clothing> options;
+
+    private String clothingId = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +43,44 @@ public class ClothingDetailsActivity extends AppCompatActivity {
                 .setQuery(query, Clothing.class)
                 .build();
 
+        // Initialize views
+        clothingImage = findViewById(R.id.image_clothing_detail);
+        clothingName = findViewById(R.id.clothing_name_detail);
+        clothingPrice = findViewById(R.id.clothing_price_detail);
+        clothingDescription = findViewById(R.id.clothing_description_detail);
 
+        number = findViewById(R.id.number);
+        btnIncrement = findViewById(R.id.btn_increment);
+        btnDecrement = findViewById(R.id.btn_decrement);
+
+        // Get clothing id from intent
+        if (getIntent() != null) {
+            clothingId = getIntent().getStringExtra("clothingId");
+        }
+        if (!clothingId.isEmpty()) {
+            getClothingDetails(clothingId);
+        }
+
+    }
+
+    private void getClothingDetails(String clothingId) {
+        clothing.child(clothingId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Clothing clothing = dataSnapshot.getValue(Clothing.class);
+                if (clothing != null) {
+                    Picasso.get().load(clothing.getImage())
+                            .into(clothingImage);
+                    clothingName.setText(clothing.getName());
+                    clothingPrice.setText(clothing.getPrice());
+                    clothingDescription.setText(clothing.getDescription());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
