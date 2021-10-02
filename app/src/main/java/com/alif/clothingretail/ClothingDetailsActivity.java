2726f72448666256a078768a78ccfa0d9b1b97c3
alifdevz <alifm.devz@gmail.com>
2021-10-02 08:24:30 +0800

    Initialize Firebase on ClothingDetailsActivity

--- app/src/main/java/com/alif/clothingretail/ClothingDetailsActivity.java
+++ app/src/main/java/com/alif/clothingretail/ClothingDetailsActivity.java
@@ -3,12 +3,35 @@ package com.alif.clothingretail;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.alif.clothingretail.model.Clothing;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.squareup.picasso.Picasso;
 
 public class ClothingDetailsActivity extends AppCompatActivity {
+    private ImageView clothingItemImage;
+
+    private FirebaseDatabase database;
+    private DatabaseReference clothing;
+    private FirebaseRecyclerOptions<Clothing> options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothing_detail);
+
+        // Initialize Firebase
+        database = FirebaseDatabase.getInstance();
+        clothing = database.getReference("clothing");
+        Query query = clothing;
+        options = new FirebaseRecyclerOptions.Builder<Clothing>()
+                .setQuery(query, Clothing.class)
+                .build();
+
+
     }
 }
