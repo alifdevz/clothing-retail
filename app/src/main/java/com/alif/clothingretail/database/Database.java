9715626db44cfc450d9bd55cb4ea91e47c3daaa5
alifdevz <alifm.devz@gmail.com>
2021-10-03 17:22:45 +0800

    Added getCarts, addToCart, and cleanCart methods in Database class

--- app/src/main/java/com/alif/clothingretail/database/Database.java
+++ app/src/main/java/com/alif/clothingretail/database/Database.java
@@ -1,10 +1,14 @@
 package com.alif.clothingretail.database;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 
+import com.alif.clothingretail.model.Order;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Database extends SQLiteAssetHelper {
@@ -13,5 +17,42 @@ public class Database extends SQLiteAssetHelper {
 
     public Database(Context context) {
         super(context, DB_NAME, null, DB_VER);
+    }
+
+    public List<Order> getCarts() {
+        SQLiteDatabase db = getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        String[] sqlSelect = {"product_name", "product_id", "quantity", "price"};
+        String sqlTable = "order_detail";
+
+        qb.setTables(sqlTable);
+        Cursor c = qb.query(db, sqlSelect, null, null, null, null, null);
+
+        final List<Order> result = new ArrayList<>();
+        if (c.moveToFirst()) {
+            do {
+                result.add(new Order(c.getString(c.getColumnIndex("product_id")),
+                        c.getString(c.getColumnIndex("product_name")),
+                        c.getString(c.getColumnIndex("quantity")),
+                        c.getString(c.getColumnIndex("price"))));
+            } while (c.moveToNext());
+        }
+        return result;
+    }
+
+    public void addToCart(Order order) {
+        SQLiteDatabase db = getReadableDatabase();
+        String query = String.format("INSERT INTO order_detail(product_id, product_name, quantity, price) VALUES('%s, %s, %s, %s');",
+                order.getProductId(),
+                order.getProductName(),
+                order.getQuantity(),
+                order.getPrice());
+        db.execSQL(query);
+    }
+
+    public void cleanCart(Order order) {
+        SQLiteDatabase db = getReadableDatabase();
+        String query = String.format("DELETE FROM order_detail;");
+        db.execSQL(query);
     }
 }
