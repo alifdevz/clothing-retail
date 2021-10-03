cd278a752dc3d7c6e9096ed70c8d1c21940391b7
alifdevz <alifm.devz@gmail.com>
2021-10-04 07:05:31 +0800

    Inflate views in CartAdapter and load OrderList in CartActivity

--- app/src/main/java/com/alif/clothingretail/CartActivity.java
+++ app/src/main/java/com/alif/clothingretail/CartActivity.java
@@ -8,8 +8,14 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.alif.clothingretail.adapter.CartAdapter;
+import com.alif.clothingretail.database.Database;
+import com.alif.clothingretail.model.Order;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CartActivity extends AppCompatActivity {
     RecyclerView rvCartItems;
@@ -20,6 +26,9 @@ public class CartActivity extends AppCompatActivity {
 
     TextView tvTotalPrice;
     Button btnPlaceOrder;
+
+    List<Order> cart = new ArrayList<>();
+    CartAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +52,15 @@ public class CartActivity extends AppCompatActivity {
     }
 
     private void loadOrderList() {
+        cart = new Database(this).getCarts();
+        adapter = new CartAdapter(cart, this);
+        rvCartItems.setAdapter(adapter);
 
+        // Calculate price total
+        int total = 0;
+        for (Order order: cart) {
+            total += (Integer.parseInt(order.getPrice())  * Integer.parseInt(order.getQuantity()));
+            tvTotalPrice.setText("Rp" + total);
+        }
     }
 }
--- app/src/main/java/com/alif/clothingretail/adapter/CartAdapter.java
+++ app/src/main/java/com/alif/clothingretail/adapter/CartAdapter.java
@@ -1,15 +1,38 @@
 package com.alif.clothingretail.adapter;
 
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.alif.clothingretail.R;
+import com.alif.clothingretail.interfaces.ItemClickListener;
+import com.alif.clothingretail.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public TextView tvCartItemName, tvCartItemPrice;
+    public ImageView imgCartItemCount;
+
+    private ItemClickListener itemClickListener;
+
+    public void setTvCartItemName(TextView tvCartItemName) {
+        this.tvCartItemName = tvCartItemName;
+    }
 
     public CartViewHolder(@NonNull View itemView) {
         super(itemView);
+        tvCartItemName = itemView.findViewById(R.id.cart_item_name);
+        tvCartItemPrice = itemView.findViewById(R.id.cart_item_price);
+        imgCartItemCount = itemView.findViewById(R.id.img_cart_item_count);
     }
 
     @Override
@@ -18,6 +41,30 @@ class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickList
     }
 }
 
-public class CartAdapter {
+public class CartAdapter extends RecyclerView.Adapter<CartViewHolder> {
+    private List<Order> orderList = new ArrayList<>();
+    private Context context;
 
+    public CartAdapter(List<Order> orderList, Context context) {
+        this.orderList = orderList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View itemView = layoutInflater.inflate(R.layout.rv_list_cart_items, parent, false);
+        return new CartViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return orderList.size();
+    }
 }
