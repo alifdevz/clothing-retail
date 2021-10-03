180234ac2e7cafdd3cda4ba286707fd17dfad130
alifdevz <alifm.devz@gmail.com>
2021-10-03 22:09:01 +0800

    Create CartAdapter containing CartViewHolder

---
+++ app/src/main/java/com/alif/clothingretail/adapter/CartAdapter.java
@@ -0,0 +1,23 @@
+package com.alif.clothingretail.adapter;
+
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public CartViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
+
+public class CartAdapter {
+
+}
