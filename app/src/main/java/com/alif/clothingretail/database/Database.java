3d7f1ac407f6430b35175f1d31346c1f0756bf96
alifdevz <alifm.devz@gmail.com>
2021-10-03 16:56:56 +0800

    Added database name, version and constructor in Database class + Added Order model with all properties (columns) like SQLite order_detail columns

--- app/src/main/java/com/alif/clothingretail/database/Database.java
+++ app/src/main/java/com/alif/clothingretail/database/Database.java
@@ -1,4 +1,17 @@
 package com.alif.clothingretail.database;
 
-public class Database {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+import java.util.List;
+
+public class Database extends SQLiteAssetHelper {
+    private static final String DB_NAME= "clothingretail.db";
+    private static final int DB_VER = 1;
+
+    public Database(Context context) {
+        super(context, DB_NAME, null, DB_VER);
+    }
 }
---
+++ app/src/main/java/com/alif/clothingretail/model/Order.java
@@ -0,0 +1,50 @@
+package com.alif.clothingretail.model;
+
+public class Order {
+    private String productId;
+    private String productName;
+    private String quantity;
+    private String price;
+
+    public Order() {
+    }
+
+    public Order(String productId, String productName, String quantity, String price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
