16efa4332e0d9eb531754aae0b2325a30f78a496
alifdevz <alifm.devz@gmail.com>
2021-10-04 21:17:23 +0800

    Initialize views and set onClickListener on itemView with itemClickListener

--- app/src/main/java/com/alif/clothingretail/viewholder/OrderViewHolder.java
+++ app/src/main/java/com/alif/clothingretail/viewholder/OrderViewHolder.java
@@ -1,4 +1,35 @@
 package com.alif.clothingretail.viewholder;
 
-public class OrderViewHolder {
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.alif.clothingretail.R;
+import com.alif.clothingretail.interfaces.ItemClickListener;
+
+public class OrderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public TextView tvOrderId, tvOrderStatus, tvOrderPhoneNumber, tvOrderAddress;
+    private ItemClickListener itemClickListener;
+
+
+    public OrderViewHolder(@NonNull View itemView) {
+        super(itemView);
+        tvOrderId = itemView.findViewById(R.id.tv_order_id);
+        tvOrderStatus = itemView.findViewById(R.id.tv_order_status);
+        tvOrderPhoneNumber = itemView.findViewById(R.id.tv_order_phone_number);
+        tvOrderAddress = itemView.findViewById(R.id.tv_order_address);
+
+        itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        itemClickListener.onClick(view, getAbsoluteAdapterPosition(), false);
+    }
 }
